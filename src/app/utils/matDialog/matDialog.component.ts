1fa719bd7caad20965aea5f6a33541598d6d6455
Manoj Kumar Desai <manojkumardbe@gmail.com>
2020-03-25 23:22:38 -0600

    Values retained in dialog

--- src/app/utils/matDialog/matDialog.component.ts
+++ src/app/utils/matDialog/matDialog.component.ts
@@ -2,10 +2,13 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Component, Inject, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ApiService } from 'src/app/_services/api.service';
 import { LoginService } from 'src/app/_services/login.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 export interface DialogData {
-    animal: string;
-    name: string;
+    group: string;
+    format: string;
+    criteria: string;
+    value: string;
 }
 
 @Component({
@@ -14,6 +17,12 @@ export interface DialogData {
 })
 export class FilterDialog implements OnInit {
     @ViewChild('downloadZipLink') private downloadZipLink: ElementRef;
+    form: FormGroup = new FormGroup({
+        group: new FormControl(''),
+        format: new FormControl(''),
+        criteria: new FormControl(''),
+        value: new FormControl(''),
+      });
     groups = [
         { value: 'County' },
         { value: 'Operator' }
@@ -41,29 +50,60 @@ export class FilterDialog implements OnInit {
         this.dialogRef.updatePosition({ top: '7.8%', left: '50px' });
         this.apiService.fetchCounties().subscribe((data) => {
             this.counties = data;
+            this.setDefaultFOrmValues();
         });
         this.apiService.fetchOperators().subscribe((data) => {
             this.operators = data;
+            this.setDefaultFOrmValues();
         });
+        this.form.setValue({
+            group: this.data && this.data.group ? this.data.group : '',
+            format: this.data && this.data.format ? this.data.format : '',
+            criteria: this.data && this.data.criteria ? this.data.criteria : '',
+            value: this.data && this.data.value ? this.data.value : '',
+        })
+        
+
         this.loginService.user.subscribe((data) => {
             this.isLoggedIn = data && data.loggedIn ? data.loggedIn : false;
         });
     }
     onNoClick(): void {
+        this.form.setValue({
+            group: '',
+            format: '',
+            criteria: '',
+            value: '',
+        })
         this.dialogRef.close();
     }
-    type(option) {
-        this.values = option.value == 'Operator' ? this.operators : this.counties;
-        this.payLoad['group'] = String(option.value).toLowerCase();
+
+    setDefaultFOrmValues() {
+        if(this.data) {
+            this.type();
+            setTimeout(()=> {
+                this.form.setValue({
+                    group: this.data && this.data.group ? this.data.group : '',
+                    format: this.data && this.data.format ? this.data.format : '',
+                    criteria: this.data && this.data.criteria ? this.data.criteria : '',
+                    value: this.data && this.data.value ? this.data.value : '',
+                });
+            }, 1000)
+            
+        }
     }
-    criteria(option) {
-        this.payLoad['criteria'] = String(option.value).toLowerCase();
+    type() {
+        this.values = this.form.value.group == 'Operator' ? this.operators : this.counties;
+        this.payLoad['group'] = this.form.value.group.toLowerCase();
     }
-    value(option) {
-        this.payLoad['value'] = String(option.value).toLowerCase();
+    criteria() {
+        this.payLoad['criteria'] = this.form.value.criteria.toLowerCase();
     }
-    fileFormat(option) {
-        this.payLoad['format'] = String(option.value).toLowerCase();
+    value() {
+        this.payLoad['value'] = this.form.value.value.toLowerCase();
+    }
+    fileFormat() {
+        this.payLoad['format'] = this.form.value.format.toLowerCase();
     }
 
     generateReport() {
