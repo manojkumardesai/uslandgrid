82a2e2cc363741347b7fbd99d7e35aa5bbcf9944
Manoj Kumar Desai <manojkumardbe@gmail.com>
2020-03-28 06:17:40 -0600

    Changed context path added environment variable

--- src/app/_services/api.service.ts
+++ src/app/_services/api.service.ts
@@ -1,56 +1,58 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
   fetchWellsData(offset, limit): Observable<any> {
-    return this.http.get(`http://mercury:8080/wells?offset=${offset}&limit=${limit}`);
+    return this.http.get(this.baseUrl + `wells?offset=${offset}&limit=${limit}`);
   }
   fetchWellsByPayLoad(payLoad, offset, limit): Observable<any> {
-    return this.http.get(`http://mercury:8080/wells?${payLoad.group}=${payLoad.value}&offset=${offset}&limit=${limit}`);
+    return this.http.get(this.baseUrl + `wells?${payLoad.group}=${payLoad.value}&offset=${offset}&limit=${limit}`);
   }
   searchWells(key): Observable<any> {
-    return this.http.get(`http://mercury:8080/wells?searchKey=${key}`);
+    return this.http.get(this.baseUrl + `wells?searchKey=${key}`);
   }
   fetchChartData(): Observable<any> {
-    return this.http.get(`http://mercury:8080/chart/county`);
+    return this.http.get(this.baseUrl + `chart/county`);
   }
   fetchWellDetails(wellId): Observable<any> {
-    return this.http.get(`http://mercury:8080/well/${wellId}`);
+    return this.http.get(this.baseUrl + `well/${wellId}`);
   }
   fetchCounties(): Observable<any> {
-    return this.http.get(`http://mercury:8080/county`);
+    return this.http.get(this.baseUrl + `county`);
   }
   fetchOperators(): Observable<any> {
-    return this.http.get(`http://mercury:8080/operator`);
+    return this.http.get(this.baseUrl + `operator`);
   }
   generateReport(payLoad) {
     // const headers = new HttpHeaders();
-    return `http://mercury:8080/report/well?state=Oklahoma&${payLoad.group}=${payLoad.value}&reportType=${payLoad.format}`;
+    return this.baseUrl + `report/well?state=Oklahoma&${payLoad.group}=${payLoad.value}&reportType=${payLoad.format}`;
   }
   fetchMcWellDetails(wellId): Observable<any> {
-    return this.http.get(`http://mercury:8080/well/mc?wellId=${wellId}`);
+    return this.http.get(this.baseUrl + `well/mc?wellId=${wellId}`);
   }
   fetchCpWellDetails(wellId): Observable<any> {
-    return this.http.get(`http://mercury:8080/well/cp?wellId=${wellId}`);
+    return this.http.get(this.baseUrl + `well/cp?wellId=${wellId}`);
   }
   fetchFtWellDetails(wellId): Observable<any> {
-    return this.http.get(`http://mercury:8080/well/ft?wellId=${wellId}`);
+    return this.http.get(this.baseUrl + `well/ft?wellId=${wellId}`);
   }
   fetchPfWellDetails(wellId): Observable<any> {
-    return this.http.get(`http://mercury:8080/well/pf?wellId=${wellId}`);
+    return this.http.get(this.baseUrl + `well/pf?wellId=${wellId}`);
   }
   fetchSurveyWellDetails(wellId): Observable<any> {
-    return this.http.get(`http://mercury:8080/well/survey?wellId=${wellId}`);
+    return this.http.get(this.baseUrl + `well/survey?wellId=${wellId}`);
   }
   fetchIpWellDetails(wellId): Observable<any> {
-    return this.http.get(`http://mercury:8080/well/ipvolume?wellId=${wellId}`);
+    return this.http.get(this.baseUrl + `well/ipvolume?wellId=${wellId}`);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://mercury:8080/uslandgrid'
 };
