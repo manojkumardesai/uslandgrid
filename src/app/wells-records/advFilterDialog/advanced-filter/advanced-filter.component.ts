046060ef14ba600d923daa0983b3245a8388973a
Manoj Kumar Desai <manojkumardbe@gmail.com>
2020-05-11 00:15:39 -0600

    Set default value to values

--- src/app/wells-records/advFilterDialog/advanced-filter/advanced-filter.component.ts
+++ src/app/wells-records/advFilterDialog/advanced-filter/advanced-filter.component.ts
@@ -208,7 +208,7 @@ export class AdvancedFilterComponent implements OnInit {
       type: 'string',
       condition: 'is',
       caseSensitive: false,
-      value: []
+      value: ''
     });
   }
 
