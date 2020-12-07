df49ff67b9cac2be89b2cb815deb23371d08f8f3
Manoj Kumar Desai <manojkumardbe@gmail.com>
2020-12-07 05:16:23 -0700

    Missing component added

---
+++ src/app/dilogs/warning-window/warning-window.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WarningWindowComponent } from './warning-window.component';
+
+describe('WarningWindowComponent', () => {
+  let component: WarningWindowComponent;
+  let fixture: ComponentFixture<WarningWindowComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WarningWindowComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WarningWindowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
