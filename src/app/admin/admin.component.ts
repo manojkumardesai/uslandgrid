f18b8c10436fd1833e9eb004b75dcf9bcec1804e
Manoj Kumar Desai <manojkumardbe@gmail.com>
2021-05-18 04:40:45 -0600

    admin board changes

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { ApiService } from "../_services/api.service";
 import { Router } from "@angular/router";
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
     selector: 'app-admin',
@@ -13,6 +16,10 @@ export class AdminComponent implements OnInit {
     users: any = [];
     count: number = 0;
     searchKey: string = '';
+    displayedColumns  = ['ind', 'id', 'firstName', 'lastName', 'emailAddress', 'status', 'subscribeValid', 'actions'];
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+    dataSource = new MatTableDataSource([])
     constructor(private _apiService: ApiService, private _router: Router) {
 
     }
@@ -27,6 +34,9 @@ export class AdminComponent implements OnInit {
     getListOfUser() {
         this._apiService.getListOfUser().subscribe((data) => {
             this.users = data['users'];
+            this.dataSource = new MatTableDataSource(this.users);
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
             this.users = this.users.sort((a, b) => a['id'] < b['id'] ? 1 : -1);
             this.count = data['count'];
         });
@@ -43,4 +53,13 @@ export class AdminComponent implements OnInit {
     gotoUser(id) {
         this._router.navigate(['/user-info/', id]);
     }
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    
+        if (this.dataSource.paginator) {
+          this.dataSource.paginator.firstPage();
+        }
+      }
 }
